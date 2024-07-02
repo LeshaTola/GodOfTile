c26564078f5d11d8308ee648ee7507ea6a9fee87
LeshaTola <tola.lesha@mail.ru>
2024-07-02 11:25:44 +0300

    Add paired Text

---
+++ Assets/App/Scripts/Features/UI/PairedTexts/PairedText.cs
@@ -0,0 +1,38 @@
+﻿using Module.Localization;
+using Module.Localization.Localizers;
+using System;
+using UnityEngine;
+
+namespace Assets.App.Scripts.Features.UI.PairedTexts
+{
+	[Serializable]
+	public class PairedText : MonoBehaviour
+	{
+		[SerializeField]
+		private TMProLocalizer header;
+
+		[SerializeField]
+		private TMProLocalizer value;
+
+		public TMProLocalizer Header
+		{
+			get => header;
+		}
+		public TMProLocalizer Value
+		{
+			get => value;
+		}
+
+		public void Init(ILocalizationSystem localizationSystem)
+		{
+			header.Init(localizationSystem);
+			value.Init(localizationSystem);
+		}
+
+		public void Translate()
+		{
+			header.Translate();
+			value.Translate();
+		}
+	}
+}
